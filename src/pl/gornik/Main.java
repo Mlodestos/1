4bfa7ade8c3d6561c33e514d1f8f5d529bbfd137
Mlodestos <kacperkogut09@gmail.com>
2024-09-16 12:27:45 +0200

    zmiana 1

--- src/pl/gornik/Main.java
+++ src/pl/gornik/Main.java
@@ -7,9 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Podaj liczbę");
+
         int number = scanner1.nextInt();
+
+        System.out.println("-------------------------");
         System.out.println(number + " To jest liczba odczytana z klawiatury");
         System.out.printf("%d to jest liczba odczytana z klawiatury\n",number);
-        System.out.println("Tekst");
+
     }
 }
